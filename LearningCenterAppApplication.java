e4e3787501787c20b271a7b928e26bab5b2edf85
ulashbaev <156516221+ulashbaev@users.noreply.github.com>
2025-02-18 12:22:00 +0500

    Add files via upload

---
+++ LearningCenterAppApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.learningcenterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCenterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningCenterAppApplication.class, args);
+    }
+
+}
